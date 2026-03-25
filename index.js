dbd48d9fda7dd934494540a4f1401d792d984803
Thiệu Trung Kiên <87187870+ttkienn@users.noreply.github.com>
2026-03-25 16:12:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var TFTTeamCode = require("./TFT.js");
 
 (async () => {
-    var result = await new TFTTeamCode().setTeamcode("0235801a03435d35b332025336331000TFTSet16").build();
+    var result = await new TFTTeamCode()
+        .setTeamcode("0235801a03435d35b332025336331000TFTSet16")
+        .build();
     console.log(`Set: ${result.setId}`);
     console.log(`Số lượng tướng: ${result.teamLength}`);
     console.table(result.team);
